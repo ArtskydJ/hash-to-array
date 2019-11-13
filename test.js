4627a212ae59ca0db3fa194d1e7d4528ec5967e3
Joseph Dykstra <25345@nelnet.net>
2019-11-13 12:35:54 -0600

    simplify tests

--- test.js
+++ test.js
@@ -1,80 +1,72 @@
 var test = require('tape')
 var hashToArray = require('./index.js')
 
-function deepEqual(t0, description, obj, expected, ignoreIndex) {
-	t0.test(description, function (t) {
+function deepEqual(description, obj, expected) {
+	test(description, function (t) {
 		var result = hashToArray(obj)
-		//console.log(expected)
-		//console.log(result)
 		t.equal(result.length, expected.length, 'lengths are equal')
 		expected.forEach(function (r, i) {
 			t.notEqual(result.indexOf(r), -1, typeof r + ' "' + r + '" found in new array')
-			if (!ignoreIndex) {
-				t.equal(i%2, result.indexOf(r)%2,
-					'both things are ' + (i%2? 'values' : 'arguments'))
-			}
+			t.equal(i%2, result.indexOf(r)%2,
+				'both things are ' + (i%2? 'values' : 'arguments'))
 		})
 		t.end()
 	})
 }
 
-test('basic argument building', function (t0) {
-	deepEqual(t0, 'short arguments', {
-		x: 2,
-		v: 50,
-		j: 'hello'
-	}, [
-		'-x', 2,
-		'-v', 50,
-		'-j', 'hello'
-	])
+// Basic argument building
 
-	deepEqual(t0, 'long arguments', {
-		rate: 48000,
-		bits: 'mp3'
-	}, [
-		'--bits', 'mp3',
-		'--rate', 48000
-	])
+deepEqual('short arguments', {
+	x: 2,
+	v: 50,
+	j: 'hello'
+}, [
+	'-x', 2,
+	'-v', 50,
+	'-j', 'hello'
+])
 
-	t0.end()
-})
+deepEqual('long arguments', {
+	rate: 48000,
+	bits: 'mp3'
+}, [
+	'--bits', 'mp3',
+	'--rate', 48000
+])
 
-test('special cases', function (t0) {
-	deepEqual(t0, 'boolean values', {
-		thingy: true,
-		anotherthingy: false,
-		kool: 'lol',
-		trixy: 'true'
-	}, [
-		'--thingy',
-		'anotherthingy',
-		'--kool', 'lol',
-		'--trixy', 'true'
-	])
+// Special cases
 
-	deepEqual(t0, 'boolean value edge cases', {
-		dashes: false,
-		'': true,
-		'-': false
-	}, [
-		'dashes',
-		'--',
-		'-'
-	])
+deepEqual('boolean values', {
+	thingy: true,
+	anotherthingy: false,
+	kool: 'lol',
+	trixy: 'true'
+}, [
+	'--thingy',
+	'anotherthingy',
+	'--kool', 'lol',
+	'--trixy', 'true'
+])
 
-	deepEqual(t0, 'array-like object', {
-		0: 8,
-		1: false,
-		length: 2
-	}, [
-		'-0', 8,
-		'1',
-		'--length', 2
-	])
+deepEqual('boolean value edge cases', {
+	dashes: false,
+	'': true,
+	'-': false
+}, [
+	'dashes',
+	'--',
+	'-'
+])
 
-	t0.end()
-})
+deepEqual('array-like object', {
+	0: 8,
+	1: false,
+	length: 2
+}, [
+	'-0', 8,
+	'1',
+	'--length', 2
+])
 
 function passThru(t, expected) {
 	var found = hashToArray(expected)
